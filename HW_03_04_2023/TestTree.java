80f47bbdb72dc81404d82efd5f02144b353c3f95
MrBizzon <info.proect@mail.ru>
2023-04-04 08:13:18 +0300

    Amended values

--- HW_03_04_2023/TestTree.java
+++ HW_03_04_2023/TestTree.java
@@ -4,27 +4,27 @@ public class TestTree {
     public static void main(String[] args) {
         BlackRedTree<String> tree = new BlackRedTree();
         tree.add("a");
-        System.out.println("-------------------------");
+        System.out.println("++++++++++++++++ Start ++++++++++++++++++");
         tree.print(tree.root);
         tree.add("b");
-        System.out.println("-------------------------");
+        System.out.println("++++++++++++++++++++++++++++++++++");
         tree.print(tree.root);
         tree.add("c");
-        System.out.println("-------------------------");
+        System.out.println("++++++++++++++++++++++++++++++++++");
         tree.print(tree.root);
         tree.add("d");
-        System.out.println("-------------------------");
-        tree.print(tree.root);
-        tree.add("f");
-        System.out.println("-------------------------");
+        System.out.println("++++++++++++++++++++++++++++++++++");
         tree.print(tree.root);
         tree.add("e");
-        System.out.println("-------------------------");
+        System.out.println("++++++++++++++++++++++++++++++++++");
         tree.print(tree.root);
-        tree.add("x");
-        System.out.println("-------------------------");
+        tree.add("f");
+        System.out.println("++++++++++++++++++++++++++++++++++");
         tree.print(tree.root);
-        System.out.println("-------------------------");
+        tree.add("k");
+        System.out.println("++++++++++++++++++++++++++++++++++");
+        tree.print(tree.root);
+        System.out.println("+++++++++++ The end ++++++++++++++");
     }
 
 }
